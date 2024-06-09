0ca6dcc0c483565debd3b157ba9726a5369c41aa
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-06-09 07:12:26 +0300

    structure of countdown

--- WEB/assets/js/controllers/gameController.js
+++ WEB/assets/js/controllers/gameController.js
@@ -14,5 +14,39 @@ gameApp.controller(
             minutes : 1,
             secondes: 0
         }
+
+    /// Functions
+        /// Demarrer le jeu
+        $scope.startGame = function(){
+            /// Changer l'etat du jeu
+            $scope.gameStatus = "run"
+        }
+        /// Transformer le chiffre en texte de temps
+        $scope.timerCaractere = function(nb){
+            return nb >= 10 ? nb : "0"+nb;
+        }
+        $scope.fullTimerCaractere = function(timer){
+            return $scope.timerCaractere(timer.hours)+":"+$scope.timerCaractere(timer.minutes)+":"+$scope.timerCaractere(timer.secondes);
+        }
+        /// recuperer le total de temps
+        $scope.timerCount=function(timer){
+            return (timer.hours*10000) + (timer.minutes*100) + (timer.secondes);
+        }
+        /// Lancer un countdown
+        $scope.countdowm = function(timer){
+            while ( timerCount(timer) > 0 ){
+                // decrease second
+                if(timer.secondes == 0 && timer.minutes > 0){
+                    timer.secondes = 59;
+                    // decrease minute
+                    if(timer.minutes == 0 && timer.minutes > 0){
+                        timer.minutes = 59;
+                        timer.hours = timer.hours > 0 ? timer.hours-1:0;
+                    }
+                    else{timer.minutes-=1}
+                }
+                else{timer.secondes-=1}
+            }
+        }
     }
 );
