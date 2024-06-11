bef021335a7b05157c837e0a4c94ff7606b99b22
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-06-11 08:21:44 +0300

    integrate javascript import

---
+++ wwwroot/js/app.js
@@ -0,0 +1,54 @@
+/// Declaration du module rally app
+let gameApp = angular.module("GameApp",["ngRoute"]);
+/// Configuration des mappings gerer dans rally app
+gameApp.config(['$routeProvider' ,  $routeProvider =>{
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'views/home/formulaire-joueur.html'
+        })
+        .when('/game', {
+            templateUrl: 'views/game.html',
+            controller: 'gameController'
+        })
+        .otherwise({
+            redirectTo:'/game'
+        });
+}]);
+gameApp.run(function($rootScope){
+    $rootScope.gameConfig ={
+        timer : {
+            hours   : 0,
+            minutes : 1,
+            secondes: 0
+        },
+        numbersIsa : 7,
+        operations : [
+            "+","-","*","/","(",")"
+        ]
+    }
+    $rootScope.players = [
+        {
+            name:'Player 1',
+            point:0,
+            validation : 'allowed'
+        },
+        {
+            name:'Player 2',
+            point:0 ,
+            validation : 'allowed'
+        }
+    ];
+})
+// CONTROLLERS
+gameApp.controller(
+    'AppController',
+    function($rootScope){
+        $rootScope.text = "NUMBER"
+    }
+);
+gameApp.controller(
+    'ControllerName',
+    function($scope,$rootScope){
+        $scope.specificNumber = "69";
+    }
+);
