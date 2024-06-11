12280ed7a3d8c77cb93b9470016634250eb33939
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-06-11 09:28:32 +0300

    restore views

--- Controllers/GamesController.cs
+++ Controllers/GamesController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ChiffresApi.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ChiffresApi.Controllers
 {
+    [Route("api/[controller]")]
     public class GamesController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet("{}")]
+        public JsonContent Create(){
+
         }
     }
 }
